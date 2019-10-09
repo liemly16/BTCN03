c1df141794dea30595b2063458eca60f6fdfa1c2
thanhliem98 <liembsbs123@gmail.com>
2019-10-09 17:14:03 +0700

    Updates

---
+++ precache-manifest.7e50d6002c7b3d05db4b1900ffb06603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2cf8bd6d039760487286b33119decd",
+    "url": "/btcn05/index.html"
+  },
+  {
+    "revision": "471f2eab625cebc94af4",
+    "url": "/btcn05/static/css/main.67081ca9.chunk.css"
+  },
+  {
+    "revision": "ace3f0303b36078e9127",
+    "url": "/btcn05/static/js/2.877283db.chunk.js"
+  },
+  {
+    "revision": "471f2eab625cebc94af4",
+    "url": "/btcn05/static/js/main.1c612dea.chunk.js"
+  },
+  {
+    "revision": "02ed568fdefa0a476611",
+    "url": "/btcn05/static/js/runtime~main.976488cf.js"
+  }
+]);
